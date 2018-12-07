c26a9a5fb876fe13a648e8fade7afbf67a9c42e9
John Ayeni <johnayeni10.aj@gmail.com>
2018-12-07 21:40:32 +0100

    feat: clone images before caching them wihh service worker

--- sw.js
+++ sw.js
@@ -37,13 +37,34 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
+  var requestUrl = new URL(event.request.url);
+
+  if (requestUrl.origin === location.origin) {
+    if (requestUrl.pathname.startsWith('/img/')) {
+      event.respondWith(servePhoto(event.request));
+      return;
+    }
+  }
+
   event.respondWith(
-    caches
-      .match(event.request, { ignoreSearch: true })
-      .then((response) => {
-        return response || fetch(event.request);
-      })
-      .catch((err) => console.log(err, event.request)),
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    }),
   );
 });
+
+function servePhoto(request) {
+  var storageUrl = request.url.replace(/-\d+px\.jpg$/, '');
+
+  return caches.open(contentImgsCache).then(function(cache) {
+    return cache.match(storageUrl).then(function(response) {
+      if (response) return response;
+
+      return fetch(request).then(function(networkResponse) {
+        cache.put(storageUrl, networkResponse.clone());
+        return networkResponse;
+      });
+    });
+  });
+}
