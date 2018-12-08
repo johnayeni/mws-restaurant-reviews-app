28076b6f66343aa39e8c0bdf1bf3629f6f2ece6b
John Ayeni <johnayeni10.aj@gmail.com>
2018-12-08 23:05:50 +0100

    feat: fetch data from server with fetch api

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -7,29 +7,24 @@ class DBHelper {
    * Change this to restaurants.json file location on your server.
    */
   static get DATABASE_URL() {
-    const port = 8000; // Change this to your server port
-    return `http://localhost:${port}/data/restaurants.json`;
+    const port = 1337; // Change this to your server port
+    return `http://localhost:${port}/restaurants`;
   }
 
   /**
    * Fetch all restaurants.
    */
-  static fetchRestaurants(callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', DBHelper.DATABASE_URL);
-    xhr.onload = () => {
-      if (xhr.status === 200) {
+  static async fetchRestaurants(callback) {
+    try {
+      const response = await fetch(DBHelper.DATABASE_URL);
+      if (response.status === 200) {
         // Got a success response from server!
-        const json = JSON.parse(xhr.responseText);
-        const restaurants = json.restaurants;
+        const restaurants = await response.json();
         callback(null, restaurants);
-      } else {
-        // Oops!. Got an error from server.
-        const error = `Request failed. Returned status of ${xhr.status}`;
-        callback(error, null);
       }
-    };
-    xhr.send();
+    } catch (error) {
+      callback(error.message, null);
+    }
   }
 
   /**
